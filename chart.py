f7f279f58dfcf22f57e5262e84c82e0d20d66824
yuvvhere911-dotcom <yuvvhere911@gmail.com>
2026-07-29 14:41:37 +0530

    Add chart.py code and chart.png

--- chart.py
+++ chart.py
@@ -1,9 +1,19 @@
+import matplotlib.pyplot as plt
 
+# Tere skills
+skills = ['HTML', 'CSS', 'Python', 'Git', 'AI']
+marks = [90, 85, 80, 75, 88]
 
+plt.figure(figsize=(8,5))
+plt.bar(skills, marks, color='skyblue')
+plt.title('My Skills Chart - Amity Agile Lab')
+plt.xlabel('Skills')
+plt.ylabel('Marks %')
+plt.ylim(0, 100)
 
-
-
-
+# Chart ko image me save karo
+plt.savefig('chart.png')
+print("chart.png ban gayi!")
 
 
 
